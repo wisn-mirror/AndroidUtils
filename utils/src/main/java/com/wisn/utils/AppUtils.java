c696257cbd0eea2a431c0cfaa8cfeb1e14ab5a2d
Wisn <wuyishun_kmk@outlook.com>
2017-10-09 21:29:51 +0800

    add AppUtils

---
+++ utils/src/main/java/com/wisn/utils/AppUtils.java
@@ -0,0 +1,195 @@
+package com.wisn.utils;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+
+import java.io.File;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wisn on 2017/10/8.
+ */
+
+public class AppUtils {
+    /**
+     * @param context
+     *
+     * @return
+     */
+    public static int getAppVersion(Context context) {
+        try {
+            return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    /**
+     * @param context
+     *
+     * @return
+     */
+    public static String getAppName(Context context) {
+        try {
+            return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).packageName;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * @param context
+     * @param file
+     */
+    public static void installApk(Context context, File file) {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(file),
+                              "application/vnd.android.package-archive");
+        context.startActivity(intent);
+    }
+
+    /**
+     * 安装apk
+     *
+     * @param context 上下文
+     * @param file    APK文件uri
+     */
+    public static void installApk(Context context, Uri file) {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setDataAndType(file, "application/vnd.android.package-archive");
+        context.startActivity(intent);
+    }
+
+    /**
+     * @param context
+     * @param packageName
+     */
+    public static void uninstallApk(Context context, String packageName) {
+        Intent intent = new Intent(Intent.ACTION_DELETE);
+        Uri packageURI = Uri.parse("package:" + packageName);
+        intent.setData(packageURI);
+        context.startActivity(intent);
+    }
+
+    /**
+     * 获取系统中所有的应用
+     *
+     * @param context 上下文
+     *
+     * @return 应用信息List
+     */
+    public static List<PackageInfo> getAllApps(Context context) {
+
+        List<PackageInfo> apps = new ArrayList<PackageInfo>();
+        PackageManager pManager = context.getPackageManager();
+        List<PackageInfo> paklist = pManager.getInstalledPackages(0);
+        for (int i = 0; i < paklist.size(); i++) {
+            PackageInfo pak = paklist.get(i);
+            if ((pak.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) <= 0)
+                apps.add(pak);
+        }
+        return apps;
+    }
+
+    /**
+     * @return
+     */
+    public static int getSDKVersion() {
+        return android.os.Build.VERSION.SDK_INT;
+    }
+
+    /**
+     * @param context
+     * @param packageName
+     *
+     * @return
+     */
+    public static String getSign(Context context, String packageName) {
+        try {
+            PackageInfo pis = context.getPackageManager()
+                                     .getPackageInfo(packageName,
+                                                     PackageManager.GET_SIGNATURES);
+            return hexdigest(pis.signatures[0].toByteArray());
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 将签名字符串转换成需要的32位签名
+     *
+     * @param paramArrayOfByte 签名byte数组
+     *
+     * @return 32位签名字符串
+     */
+    private static String hexdigest(byte[] paramArrayOfByte) {
+        final char[] hexDigits = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97,
+                                  98, 99, 100, 101, 102};
+        try {
+            MessageDigest localMessageDigest = MessageDigest.getInstance("MD5");
+            localMessageDigest.update(paramArrayOfByte);
+            byte[] arrayOfByte = localMessageDigest.digest();
+            char[] arrayOfChar = new char[32];
+            for (int i = 0, j = 0; ; i++, j++) {
+                if (i >= 16) {
+                    return new String(arrayOfChar);
+                }
+                int k = arrayOfByte[i];
+                arrayOfChar[j] = hexDigits[(0xF & k >>> 4)];
+                arrayOfChar[++j] = hexDigits[(k & 0xF)];
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public static long getMaxMemory() {
+        return Runtime.getRuntime().maxMemory() >> 10;
+    }
+
+    /**
+     * @return
+     */
+    public static long getFreeMemory() {
+        return Runtime.getRuntime().freeMemory() >> 10;
+    }
+
+    /**
+     * @return
+     */
+    public static long getTotalMemory() {
+        return Runtime.getRuntime().totalMemory() >> 10;
+    }
+
+    /**
+     * @param context
+     *
+     * @return
+     */
+    public static int getDeviceUsableMemory(Context context) {
+        ActivityManager am = (ActivityManager) context.getSystemService(
+                Context.ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
+        am.getMemoryInfo(mi);
+        // 返回当前系统的可用内存
+        return (int) (mi.availMem >> 10);
+    }
+}
