7102719f6a2c34c3da2bb8f5e03c96486ca32b0e
Wisn <wuyishun_kmk@outlook.com>
2017-10-09 21:42:42 +0800

    add MD5Utils and ShortUtils

---
+++ utils/src/main/java/com/wisn/utils/MD5Utils.java
@@ -0,0 +1,72 @@
+package com.wisn.utils;
+
+import android.text.TextUtils;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+
+/**
+ * Created by wisn on 2017/10/9.
+ */
+
+public class MD5Utils {
+    private static final char HEX_DIGITS[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                                              'A', 'B', 'C', 'D', 'E', 'F'};
+
+    private static String toHexString(byte[] b) {
+        StringBuilder sb = new StringBuilder(b.length * 2);
+        for (int i = 0; i < b.length; i++) {
+            sb.append(HEX_DIGITS[(b[i] & 0xf0) >>> 4]);
+            sb.append(HEX_DIGITS[b[i] & 0x0f]);
+        }
+        return sb.toString();
+    }
+
+    public static String getMD5ByFile(String filePath) {
+        InputStream fis;
+        byte[] buffer = new byte[1024];
+        int numRead = 0;
+        MessageDigest md5;
+        try {
+            fis = new FileInputStream(filePath);
+            md5 = MessageDigest.getInstance("MD5");
+            while ((numRead = fis.read(buffer)) > 0) {
+                md5.update(buffer, 0, numRead);
+            }
+            fis.close();
+            String md5Str = toHexString(md5.digest());
+            return TextUtils.isEmpty(md5Str) ? "" : md5Str;
+        } catch (Exception e) {
+            System.out.println("error");
+            return "";
+        }
+    }
+
+    public static String getMD5ByStr(String string) {
+        String result = null;
+        try {
+            char[] charArray = string.toCharArray();
+            byte[] byteArray = new byte[charArray.length];
+            for (int i = 0; i < charArray.length; i++) {
+                byteArray[i] = (byte) charArray[i];
+            }
+
+            StringBuffer hexValue = new StringBuffer();
+            byte[] md5Bytes = MessageDigest.getInstance("MD5")
+                                           .digest(byteArray);
+            for (int i = 0; i < md5Bytes.length; i++) {
+                int val = ((int) md5Bytes[i]) & 0xff;
+                if (val < 16) {
+                    hexValue.append("0");
+                }
+                hexValue.append(Integer.toHexString(val));
+            }
+
+            result = hexValue.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
