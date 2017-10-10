d8e7e2b32f84ddd57d4a253fe8011f3eb58637b5
Wisn <wuyishun_kmk@outlook.com>
2017-10-10 10:35:21 +0800

    update DensityUtils

--- utils/src/main/java/com/wisn/utils/DensityUtils.java
+++ utils/src/main/java/com/wisn/utils/DensityUtils.java
@@ -1,12 +1,20 @@
 package com.wisn.utils;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Point;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
 
 /**
  * Created by wisn on 2017/6/22.
  */
 
 public class DensityUtils {
+
     /**
      * 根据手机的分辨率从 dip 的单位 转成为 px(像素)
      */
@@ -21,5 +29,37 @@ public class DensityUtils {
     public static int px2dip(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
+    }
+
+    private static int[] deviceWidthHeight = new int[2];
+
+    public static int[] getDeviceInfo(Context context) {
+        if ((deviceWidthHeight[0] == 0) && (deviceWidthHeight[1] == 0)) {
+            DisplayMetrics metrics = new DisplayMetrics();
+            ((Activity) context).getWindowManager().getDefaultDisplay()
+                                .getMetrics(metrics);
+
+            deviceWidthHeight[0] = metrics.widthPixels;
+            deviceWidthHeight[1] = metrics.heightPixels;
+        }
+        return deviceWidthHeight;
+    }
+
+
+    /**
+     * 获取屏幕尺寸
+     */
+    @SuppressWarnings("deprecation")
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    public static Point getScreenSize(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB_MR2) {
+            return new Point(display.getWidth(), display.getHeight());
+        } else {
+            Point point = new Point();
+            display.getSize(point);
+            return point;
+        }
     }
 }
